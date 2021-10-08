1c92b768efce530b9afcb4f4a57da988cb64e5e2
Айнур Самигулов <ARSamigulov@stud.kpfu.ru>
2021-10-08 10:37:06 +0300

    Добавьте файлы проекта.

---
+++ BDConnectingApp/Forum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BDConnectingApp
+{
+    class Forum
+    {
+        private int postid;
+        private string threadname;
+        private int userid;
+        private string creationDate;
+        private string title;
+        private string body;
+
+        public int Postid { get => postid; set => postid = value; }
+        public string Threadname { get => threadname; set => threadname = value; }
+        public int Userid { get => userid; set => userid = value; }
+        public string CreationDate { get => creationDate; set => creationDate = value; }
+        public string Title { get => title; set => title = value; }
+        public string Body { get => body; set => body = value; }
+    }
+
+    
+}
---
+++ BDConnectingApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BDConnectingApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Forum newforum1 = new Forum
+            {
+                Postid = 2,
+                Threadname = "Новая тема",
+                Userid = 1,
+                CreationDate = "08.10.2021",
+                Title = "Имя нового сообщения",
+                Body = "Новое сообщение",
+            };
+
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=BuisnessDataBase;Integrated Security=True";
+            string sqlExpression = "INSERT INTO FORUM (postid, threadname, userid, creationDate, title, body) VALUES (@postid, @threadname, @userid, @creationDate, @title, @body)";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlParameter postidParam = new SqlParameter(@"postid", newforum1.Postid);
+                command.Parameters.Add(postidParam);
+                SqlParameter threadnameParam = new SqlParameter("@threadname", newforum1.Threadname);
+                command.Parameters.Add(threadnameParam);
+                SqlParameter useridParam = new SqlParameter("@userid", newforum1.Userid);
+                command.Parameters.Add(useridParam);
+                SqlParameter creationDateParam = new SqlParameter("@creationDate", newforum1.CreationDate);
+                command.Parameters.Add(creationDateParam);
+                SqlParameter titleParam = new SqlParameter("@title", newforum1.Title);
+                command.Parameters.Add(titleParam);
+                SqlParameter bodyParam = new SqlParameter("@body", newforum1.Body);
+                command.Parameters.Add(bodyParam);
+                int number = command.ExecuteNonQuery();
+                Console.WriteLine("Добавлено объектов: {0}", number);
+            }
+        }
+    }
+}
